520ee21f203a54a3974a90d94c3bbc1738ff2eb0
Andrea Tacchetti <atacchet@frank.local>
2013-04-09 19:16:29 -0400

    config files

--- src/main.py
+++ src/main.py
@@ -1,13 +1,17 @@
 from PyQt4 import QtCore, QtGui
+from utils.utils import utils
 import sys
 from rpc.peer import Peer
 
 if __name__ == '__main__':
     # init QtGUI
+    config = utils.getConfig()
     app = QtGui.QApplication(sys.argv)
 
     # init node 
-    peer = Peer('localhost',8000)
+    ip = config.get(sys.argv[1],'ip');
+    port = int(config.get(sys.argv[1],'port'))
+    peer = Peer(ip,port)
 
     sys.exit(app.exec_())
 
---
+++ src/utils/utils.py
@@ -0,0 +1,18 @@
+import ConfigParser
+import os
+
+class utils:
+	def __init__(self):
+		pass
+	@staticmethod
+	def getConfig():
+		config = ConfigParser.RawConfigParser()
+		for loc in os.listdir("%s/conf" % os.environ.get('DW_BASE')):
+			config.read("%s/%s/%s" % (os.environ.get('DW_BASE'),'conf',loc))
+		return config
+	@staticmethod
+	def getLogPath(logtype):
+		config = utils.getConfig()
+		return "%s/%s" % (os.environ.get('DW_BASE'), config.get('log', logtype))
+		
+		

756861e47cb14a0eeee68bb22c55deef07e41fef
Andrea Tacchetti <atacchet@frank.local>
2013-04-09 19:29:59 -0400

    remove ignored files

--- src/main.py
+++ src/main.py
@@ -6,9 +6,12 @@ from rpc.peer import Peer
 if __name__ == '__main__':
     # init QtGUI
     config = utils.getConfig()
+    sys.stdout = open(utils.getLogPath('mainlog'),'a')
+    sys.stderr = open(utils.getLogPath('errorlog'),'a')
     app = QtGui.QApplication(sys.argv)
 
     # init node 
+
     ip = config.get(sys.argv[1],'ip');
     port = int(config.get(sys.argv[1],'port'))
     peer = Peer(ip,port)

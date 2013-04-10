447d9c33755ce8b5d74c3e4471fc2164236fc5ce
Andrea Tacchetti <andreatacchetti@gmail.com>
2013-04-09 20:03:13 -0400

    boobies

--- src/main.py
+++ src/main.py
@@ -2,12 +2,17 @@ from PyQt4 import QtCore, QtGui
 from dp.src.utils.utils import utils
 import sys
 from dp.src.rpc.peer import Peer
+import datetime
 
 if __name__ == '__main__':
     # init QtGUI
+    n = datetime.datetime.now().__str__()
     config = utils.getConfig()
     sys.stdout = open(utils.getLogPath('mainlog'),'a')
     sys.stderr = open(utils.getLogPath('errorlog'),'a')
+
+    print "New Run: %s\n" % n
+    sys.stderr.write("New Run: %s\n" % n)
     app = QtGui.QApplication(sys.argv)
 
     # init node 

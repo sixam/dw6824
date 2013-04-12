5ac6c9a83c1a0587cf73838a309ffeebd83b8a6f
Andrea Tacchetti <andreatacchetti@gmail.com>
2013-04-11 20:46:44 -0400

    p

--- src/rpc/priority.py
+++ src/rpc/priority.py
@@ -5,43 +5,50 @@ class Priority:
 
 	Assumes that caller holds the lock on the state.
 	"""
-	def __init__(self, rq, state):
+	def __init__(self, rq=None, state=None, pd=[]):
+		if pd:
+			self.pd = pd
+			return
 		self.pd = []
 		for qrq in state.queue:
-			if qrq.op.opos == op.opos:
-				if Priority.compareLists(self.pd, qrq.priority.pd) == 2:
+			if qrq.op.opos == rq.op.opos:
+				if Priority.compareLists(self.pd, qrq.priority.pd) < 0:
 					self.pd = qrq.priority.pd[:]
 		self.pd.append(self.state.id);
+
+	def __cmp__(self,other):
+		return Priority.compareLists(self.pd, other.pd)
+		
 
 	@staticmethod
 	def compareLists(a, b):
 		# 0 a == b
 		# 1 a > b
-		# 2 a < b
+		# -1 a < b
 		c = 0
 		if not a and not b:
 			return 0
 		if not b:
 			return 1
 		if not a:
-			return 2
+			return -1
 		if len(a) == len(b):
 			m = len(a)
 		elif len(a) < len(b):
 			m = len(a)
-			c = 1
+			c = -1
 		else:
 			m = len(b)
-			c = 2
+			c = 1
 
 		for k in range(m):
 			if a[k] > b[k]:
 				return 1
 			if a[k] < b[k]:
-				return 2
+				return -1
 		if c == 0:
 			return 0 # the two strings are identical
-		if c == 1:
-			return 2 # b is a substring of a
+		if c == -1:
+			return -1 # b is a substring of a
 		else:
 			return 1

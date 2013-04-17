c59eed790547bb71aeec82309aceef45e041dfdb
Michael Gharbi <gharbi@mit.edu>
2013-04-17 15:05:57 -0400

    bla

--- src/rpc/common.py
+++ src/rpc/common.py
@@ -62,14 +62,14 @@ class PeerState(QtCore.QObject):
                 if cmp==-1:
                     mr_i = self.mostRecent(rq.vt)
                     while True:
-                        if mr_i >= 0:
+                        if mr_i >= 0 and mr_i < len(self.log):
                             mr = self.log[mr_i]
                         else:
                             mr = None
                         if not mr or rq.op.type == OpType.NoOp:
                             break
-                        mr_i -= 1
-                        print 'MR is ', mr
+                        mr_i += 1
+                        print 'MR is ',mr_i, mr
                         if rq.vt[mr.sender] <= mr.vt[mr.sender]:
                             self.transform(rq,mr)
 

543c915e970ab65d35c8cb61fd31924cf3b73aa1
Andrea Tacchetti <andreatacchetti@gmail.com>
2013-04-19 02:06:43 -0400

    context stub

--- src/rpc/context.py
+++ src/rpc/context.py
@@ -1,6 +1,11 @@
+from dp.src.rpc.cot import COT
 class Context:
-    def __init__(self):
-        pass
-    
-
-
+    def __init__(self, c = []):
+        self.c
+    def __cmp__(self, other):
+        if len(self) > len(other) and COT.issublist(other.c, self.c):
+            return 1
+        elif len(self) < len(other) and COT.issublist(self.c, other.c):
+            return -1
+        else:
+            return 0

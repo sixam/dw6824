4a0395d995880bd633db5c0d3c5398ef69ffadec
Andrea Tacchetti <andreatacchetti@gmail.com>
2013-04-23 20:55:06 -0400

    start/join not tested

--- src/session/test.py
+++ src/session/test.py
@@ -5,27 +5,23 @@ from dp.src.utils.log import Log
 
 if __name__ == '__main__':
     sp              = 9005
-    cp              = 8005
-    ncp             = 7005
+    pbase           = 8005
 
     lc              = Log(100)
     cs              = CentralServer('localhost', sp, lc)
     csrv            = xmlrpclib.Server('http://%s:%s' % ('localhost', sp))
 
-    ld              = Log(50) 
-    dc              = DummyClient('localhost', cp, ld, csrv)
-    dsrv            = xmlrpclib.Server('http://%s:%s' % ('localhost',cp))
-
-    cs.hosts        = [['localhost']]
-    cs.ports        = [[cp]]
-    cs.participants = [[dsrv]]
-
-    lncd            = Log(42)
-    csrvndc         = xmlrpclib.Server('http://%s:%s' % ('localhost', sp))
-    ndc             = DummyClient('localhost', ncp, lncd, csrvndc)
-
-
-
-    dc.cstart('localhost', cp)
-#    dc.cjoin(0,'localhost',cp)
-    ndc.cjoin(0,'localhost', ncp)
+    n = 4
+    dc = []
+    cssrv = []
+    l = []
+    for i in range(4):
+        l.append(Log(i))
+        cssrv.append(xmlrpclib.Server('http://%s:%s' % ('localhost', sp)))
+        dc.append(DummyClient('localhost', pbase + i, l[i], cssrv[i])
+    sessions = []
+    for i in range(floor(n/2)):
+        sessions.append(dc[i].cstart('localhost', pbase + 1))
+    m = len(sessions)
+    for i in range(m,n):
+        dc[i].cjoin(session[i - m], 'localhost',pbase + i)

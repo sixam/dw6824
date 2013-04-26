1a68aa55a270eee0368b5f7cb923f695108b678b
Michael Gharbi <gharbi@mit.edu>
2013-04-26 17:00:58 -0400

    test

---
+++ test/test_central.py
@@ -0,0 +1,17 @@
+import unittest
+import sys
+import time
+import random
+
+from PyQt4 import QtCore, QtGui
+
+from dp.src.utils.log import Log
+from dp.src.utils.utils import Utils
+from dp.src.rpc.peer import Peer
+from dp.src.ui.stroke import Stroke
+from dp.src.rpc.clerk import Clerk
+
+from .test_common import GenericTestCase
+
+class CentralServerTest()
+    pass
